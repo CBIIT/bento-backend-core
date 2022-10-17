4af2210dca7fdb79ce234c5ab9334514439fde97
knockknockyoo <iheaven0129@gmail.com>
2022-10-17 16:44:29 -0400

    Revert "Differentiated public and private global search API"
    This reverts commit be1006d132115d598d333aa7a01dd0b899f0a747.

--- model/search/yaml/type/AbstractYamlType.java
+++ model/search/yaml/type/AbstractYamlType.java
@@ -1,6 +1,5 @@
 package gov.nih.nci.bento.model.search.yaml.type;
 
-import gov.nih.nci.bento.constants.Const;
 import gov.nih.nci.bento.model.search.query.QueryParam;
 import gov.nih.nci.bento.model.search.yaml.IFilterType;
 import gov.nih.nci.bento.model.search.yaml.ITypeQuery;
@@ -19,9 +18,5 @@ public abstract class AbstractYamlType {
                 .args(env.getArguments())
                 .outputType(env.getFieldType())
                 .build();
-    }
-
-    protected String getYamlFileName(Const.ES_ACCESS_TYPE accessType, String fileName) {
-        return accessType.equals(Const.ES_ACCESS_TYPE.PUBLIC) ? "public_" + fileName : fileName;
     }
 }
