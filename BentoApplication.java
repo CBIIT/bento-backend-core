3997d04e12f4941587c7a6d8a866523ec217a6f1
David-YuWei <ywwei85@gmail.com>
2024-12-20 16:43:24 -0500

    Add debug info

--- BentoApplication.java
+++ BentoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 
 @SpringBootApplication(scanBasePackages = {"gov.nih.nci"})
 public class BentoApplication extends SpringBootServletInitializer {
@@ -13,6 +16,32 @@ public class BentoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BentoApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Get the Java Runtime object
+        Runtime runtime = Runtime.getRuntime();
+
+        // Get the maximum heap size (in bytes)
+        long maxMemory = runtime.maxMemory();
+        // Get the initial heap size (in bytes)
+        long initialMemory = runtime.totalMemory();
+        // Get the current available memory (in bytes)
+        long freeMemory = runtime.freeMemory();
+
+        // Convert to MB for better readability
+        System.out.println("Initial Heap Size: " + (initialMemory / (1024 * 1024)) + " MB");
+        System.out.println("Maximum Heap Size: " + (maxMemory / (1024 * 1024)) + " MB");
+        System.out.println("Free Memory: " + (freeMemory / (1024 * 1024)) + " MB");
+
+        // Optionally log the memory usage using MemoryMXBean
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+        
+        System.out.println("Used Heap Memory: " + (heapMemoryUsage.getUsed() / (1024 * 1024)) + " MB");
+        System.out.println("Committed Heap Memory: " + (heapMemoryUsage.getCommitted() / (1024 * 1024)) + " MB");
+    }
+
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
