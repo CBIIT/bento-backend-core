e8001901a0386027e4ccc75d2e4d504325ae5368
Joon Lee <joon.lee2@nih.gov>
2025-10-28 11:37:39 -0400

    Revert "Increased Opensearch timeouts"
    This reverts commit 060059660924805471b1d72697e5671665ef0d5d.

--- service/connector/AWSClient.java
+++ service/connector/AWSClient.java
@@ -7,9 +7,7 @@ import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
 import gov.nih.nci.bento.model.ConfigurationDAO;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.client.config.RequestConfig;
 import org.opensearch.client.RestClient;
-import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
 
 public class AWSClient extends AbstractClient {
@@ -27,14 +25,9 @@ public class AWSClient extends AbstractClient {
         signer.setRegionName(config.getRegion());
         HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(config.getServiceName(), signer, credentialsProvider);
 
-        RestClientBuilder builder = RestClient.builder(new HttpHost(config.getEsHost().trim(), config.getEsPort(), config.getEsScheme())).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor))
-                // Per-connection/request defaults:
-                .setRequestConfigCallback((RequestConfig.Builder b) -> b
-                        .setConnectTimeout(5_000)              // TCP connect timeout
-                        .setConnectionRequestTimeout(5_000)    // wait for a connection from pool
-                        .setSocketTimeout(120_000));           // no-bytes-read timeout
-
-        return new RestHighLevelClient(builder);
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost(config.getEsHost().trim(), config.getEsPort(), config.getEsScheme())).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
     }
 
     @Override
@@ -43,12 +36,6 @@ public class AWSClient extends AbstractClient {
         signer.setServiceName(config.getServiceName());
         signer.setRegionName(config.getRegion());
         HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(config.getServiceName(), signer, credentialsProvider);
-        return RestClient.builder(new HttpHost(config.getEsHost().trim(), config.getEsPort(), config.getEsScheme())).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor))
-                // Per-connection/request defaults:
-                .setRequestConfigCallback((RequestConfig.Builder b) -> b
-                        .setConnectTimeout(5_000)              // TCP connect timeout
-                        .setConnectionRequestTimeout(5_000)    // wait for a connection from pool
-                        .setSocketTimeout(120_000))           // no-bytes-read timeout
-                .build();
+        return RestClient.builder(new HttpHost(config.getEsHost().trim(), config.getEsPort(), config.getEsScheme())).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)).build();
     }
 }
