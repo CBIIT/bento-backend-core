853473711faa69ea02657f762aa2e75d3db7a4c9
Eric Miller <ermiller@alumni.cmu.edu>
2026-04-14 13:55:55 -0400

    fix: avoid hardcoding SameSite attribute

--- TomcatConfig.java
+++ TomcatConfig.java
@@ -2,6 +2,7 @@ package gov.nih.nci.bento;
 
 import org.apache.catalina.valves.ErrorReportValve;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfig {
 
+    @Value("${server.cookie.same-site:Lax}")
+    private String sameSite;
+
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
         return factory -> factory.addContextCustomizers(context -> {
             Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
-            cookieProcessor.setSameSiteCookies("Lax");
+            cookieProcessor.setSameSiteCookies(sameSite);
             context.setCookieProcessor(cookieProcessor);
 
             for (org.apache.catalina.Valve valve : context.getParent().getPipeline().getValves()) {
