7534f0cc21d0d006f33877e85921373e4faa49a0
Eric Miller <ermiller@alumni.cmu.edu>
2026-04-14 12:31:30 -0400

    fix: add SameSite attribute to Path cookie

--- TomcatConfig.java
+++ TomcatConfig.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.bento;
 
 import org.apache.catalina.valves.ErrorReportValve;
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,10 @@ public class TomcatConfig {
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
         return factory -> factory.addContextCustomizers(context -> {
+            Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies("Lax");
+            context.setCookieProcessor(cookieProcessor);
+
             for (org.apache.catalina.Valve valve : context.getParent().getPipeline().getValves()) {
                 if (valve instanceof ErrorReportValve errorReportValve) {
                     errorReportValve.setShowReport(false);
