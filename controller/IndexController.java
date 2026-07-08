956c277bfc5d27f6fc290ff0d6014f593ba8ab72
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2026-07-08 16:20:48 -0400

    Revert "Potential fix for pull request finding"
    This reverts commit ac4ebea8aedcc7bf697eae6cd3594f7bcab6927d.

--- controller/IndexController.java
+++ controller/IndexController.java
@@ -32,12 +32,12 @@ public class IndexController {
 	}
 
 	@CrossOrigin
-	@RequestMapping(value = "/health", method = RequestMethod.GET,
-			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
+	@RequestMapping(value = "/health", method = RequestMethod.GET)
 	@ResponseBody
-	public org.springframework.http.ResponseEntity<String> health() {
-		return org.springframework.http.ResponseEntity.ok()
-				.contentType(org.springframework.http.MediaType.APPLICATION_JSON)
-				.body("{\"status\":\"UP\"}");
+	public String health(HttpServletRequest request, HttpServletResponse response) {
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		return "{\"status\":\"UP\"}";
+
 	}
 }
